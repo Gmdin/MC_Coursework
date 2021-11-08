363b297c809cf4986046b651fcd0bfdf881d7835
gmdin <gmdin716@gmail.com>
2021-11-09 00:06:46 +0500

    HomePage Completed

--- app/src/main/java/com/example/mc_assignment3_bsef18a539/MainActivity.java
+++ app/src/main/java/com/example/mc_assignment3_bsef18a539/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.mc_assignment3_bsef18a539;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+        Button btnRepo,btnApp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnRepo=(Button) findViewById(R.id.btnRepo);
+        btnApp=(Button) findViewById(R.id.btnApp);
+
+        btnRepo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openWebPage("https://github.com/Gmdin/MC_Coursework/commits/main");
+            }
+            public void openWebPage(String url) {
+                Uri webpage = Uri.parse(url);
+                Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(intent);
+            }
+        });
+        btnApp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,HomePage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
