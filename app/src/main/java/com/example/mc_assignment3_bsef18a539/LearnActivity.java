dfa794aa504e1d012dbe5786e321948aa0b1489e
gmdin <gmdin716@gmail.com>
2021-11-09 22:46:56 +0500

    completed learnPage of Lahatiyah with Landscape

--- app/src/main/java/com/example/mc_assignment3_bsef18a539/LearnActivity.java
+++ app/src/main/java/com/example/mc_assignment3_bsef18a539/LearnActivity.java
@@ -2,13 +2,35 @@ package com.example.mc_assignment3_bsef18a539;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LearnActivity extends AppCompatActivity {
+
+    Button btnHalqiyah,btnLahatiyah;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn);
+
+    btnHalqiyah=(Button) findViewById(R.id.btnHalqiyah);
+        btnLahatiyah=(Button) findViewById(R.id.btnLahatiyah);
+        btnHalqiyah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LearnActivity.this,learn_Halqiyah.class);
+                startActivity(intent);
+            }
+        });
+        btnLahatiyah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(LearnActivity.this,learn_Lahatiyah.class);
+                startActivity(intent);
+            }
+        });
     }
 }
