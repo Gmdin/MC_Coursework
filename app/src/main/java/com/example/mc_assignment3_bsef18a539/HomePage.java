1dfa524e7449f421316b768652403a1f61724a72
gmdin <gmdin716@gmail.com>
2021-11-09 11:23:03 +0500

    Complete LearnActivity activity

--- app/src/main/java/com/example/mc_assignment3_bsef18a539/HomePage.java
+++ app/src/main/java/com/example/mc_assignment3_bsef18a539/HomePage.java
@@ -2,13 +2,28 @@ package com.example.mc_assignment3_bsef18a539;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
+
+    Button btnLearn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        btnLearn=(Button) findViewById(R.id.btnLearn);
+
+        btnLearn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(HomePage.this,LearnActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
